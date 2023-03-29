ffbf5054b7be70fb9f08d06bba22bee50de840c0
TrinhHungQuan <trinhhungquan12345@gmail.com>
2023-03-29 10:17:34 +0700

    v2

--- Testing/Form1.Designer.cs
+++ Testing/Form1.Designer.cs
@@ -29,25 +29,38 @@
         private void InitializeComponent()
         {
             this.username = new System.Windows.Forms.Label();
+            this.btnGet = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // username
             // 
             this.username.AutoSize = true;
             this.username.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
-            this.username.Location = new System.Drawing.Point(135, 28);
+            this.username.Location = new System.Drawing.Point(202, 43);
+            this.username.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.username.Name = "username";
-            this.username.Size = new System.Drawing.Size(537, 29);
+            this.username.Size = new System.Drawing.Size(799, 40);
             this.username.TabIndex = 0;
             this.username.Text = "Click the button below to recieve free scholarship";
             // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(469, 189);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(204, 133);
+            this.btnGet.TabIndex = 1;
+            this.btnGet.Text = "Get";
+            this.btnGet.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.btnGet);
             this.Controls.Add(this.username);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -58,6 +71,7 @@
         #endregion
 
         private System.Windows.Forms.Label username;
+        private System.Windows.Forms.Button btnGet;
     }
 }
 
